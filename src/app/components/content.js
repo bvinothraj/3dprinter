6e778f93a3952ec800b025e74f1eacf4277501f9
Vinoth Raj <vinothr@mindfiresolutions.com>
2018-11-19 13:17:40 +0530

    Added text to home page

--- src/app/components/content.js
+++ src/app/components/content.js
@@ -6,7 +6,9 @@ import React from 'react'
 export default class Content extends React.Component {
 	render() {
 		return(
-			<section className="container body"></section>
+			<section>
+				<h1 className="bigtext">Welcome to 3DP!</h1>
+			</section>
 		);
 	}
 }
